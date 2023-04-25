c920689939afd13bddf834f6ab2f7bebc765a78f
Sabrina.Peoples <sabrina.peoples@red-gate.com>
2023-04-25 12:01:24 +0100

    Turn method

--- MarsRoverKata/Rover.cs
+++ MarsRoverKata/Rover.cs
@@ -17,10 +17,35 @@ public class Rover
     {
         foreach (var movement in movements)
         {
-            if (Direction == Direction.N)
+            // if (movements == Movement.F)
+            // {
+            //     Location += new Vector2(0, 1);
+            // }
+            // else if (Direction == Direction.S)
+            // {
+            //     Location += new Vector2(0, -1);
+            // }
+            if ((movement == Movement.L) || (movement == Movement.R))
             {
-                Location += new Vector2(0, 1);
+                Direction = Turn(movement);
             }
+        }
+    }
+
+    private Direction Turn(Movement movement)
+    {
+        switch (Direction)
+        {
+            case Direction.N :
+                return movement == Movement.L ? Direction.W : Direction.E;
+            case Direction.S :
+                return movement == Movement.L ? Direction.E : Direction.W;  
+            case Direction.E :
+                return movement == Movement.L ? Direction.N : Direction.S; 
+            case Direction.W :
+                return movement == Movement.L ? Direction.S : Direction.N; 
+            default:
+                return Direction;
         }
     }
 }
--- MarsRoverKata/UnitTest1.cs
+++ MarsRoverKata/UnitTest1.cs
@@ -10,7 +10,25 @@ public class Tests
         var rover = new Rover(new Vector2(1, 1), Direction.N);
         rover.Move(new [] { Movement.F });
 
-        Assert.Equals(rover.Location.X, 1);
-        Assert.Equals(rover.Location.Y, 2);
+        Assert.That(rover.Location.X, Is.EqualTo(1));
+        Assert.That(rover.Location.Y, Is.EqualTo(2));
+    }
+    
+    [TestCase(Direction.N, new [] { Movement.L }, Direction.W)]
+    [TestCase(Direction.N, new [] { Movement.R }, Direction.E)]
+    [TestCase(Direction.S, new [] { Movement.L }, Direction.E)]
+    [TestCase(Direction.S, new [] { Movement.R }, Direction.W)]
+    [TestCase(Direction.W, new [] { Movement.L }, Direction.S)]
+    [TestCase(Direction.W, new [] { Movement.R }, Direction.N)]
+    [TestCase(Direction.E, new [] { Movement.L }, Direction.N)]
+    [TestCase(Direction.E, new [] { Movement.R }, Direction.S)]
+    public void TestTurnsInDirection(Direction initDirection, IEnumerable<Movement> movements, Direction xVectorDirection)
+    {
+        var rover = new Rover(new Vector2(1, 1), initDirection);
+        rover.Move(movements);
+
+        Assert.That(rover.Location.X, Is.EqualTo(1));
+        Assert.That(rover.Location.Y, Is.EqualTo(1));
+        Assert.That(rover.Direction, Is.EqualTo(xVectorDirection));
     }
 }
